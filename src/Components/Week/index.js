59e6f62c513727be40b71fa7778309317f9bde06
Tony Boules <tboules@gmail.com>
2020-09-18 00:17:59 -0700

    version 1.0

--- src/Components/Week/index.js
+++ src/Components/Week/index.js
@@ -4,11 +4,36 @@ import "./Week.css";
 import { getThisWeek } from "./dates";
 import moment from "moment";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import { motion, AnimatePresence } from "framer-motion";
 
 // use framer motion to animate the carousel
 
+const variants = {
+  enter: (direction) => {
+    return {
+      x: direction === "right" ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction) => {
+    return {
+      zIndex: 0,
+      x: direction === "left" ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+};
+
 const Week = ({ finishItems }) => {
-  const [currentWeek, setCurrentWeek] = React.useState(new Date());
+  const [[currentWeek, direction], setCurrentWeek] = React.useState([
+    new Date(),
+    null,
+  ]);
   const [weekCards, setWeekCards] = React.useState([
     { id: 1, day: "Sunday", finishedItems: [] },
     { id: 2, day: "Monday", finishedItems: [] },
@@ -52,11 +77,11 @@ const Week = ({ finishItems }) => {
   }, [currentWeek, finishItems]);
 
   const goToPreviousWeek = () => {
-    setCurrentWeek(moment(currentWeek).subtract(1, "weeks").toDate());
+    setCurrentWeek([moment(currentWeek).subtract(1, "weeks").toDate(), "left"]);
   };
 
   const goToNextWeek = () => {
-    setCurrentWeek(moment(currentWeek).add(1, "weeks").toDate());
+    setCurrentWeek([moment(currentWeek).add(1, "weeks").toDate(), "right"]);
   };
 
   const totalHours = (arr) => {
@@ -65,7 +90,8 @@ const Week = ({ finishItems }) => {
     }, 0);
   };
 
-  console.log(moment(currentWeek).subtract(1, "weeks").week());
+  console.log(currentWeek);
+  console.log(moment(currentWeek).week());
 
   return (
     <div className="weekContainer">
@@ -78,30 +104,46 @@ const Week = ({ finishItems }) => {
           <FaAngleRight className="arrow" />
         </button>
       </div>
-      <div className="weekBar">
-        {weekCards.map((weekday) => {
-          return (
-            <div className="weekCard" key={weekday.id}>
-              <label className={`days ${weekday.day}`}>{weekday.day}</label>
-              <ul className="weekList">
-                {weekday.finishedItems.map((item) => {
-                  return (
-                    <li className="weekListItem" key={item.id}>
-                      {item.todo}
-                      <div className="numHours">{item.completionTime}</div>
-                    </li>
-                  );
-                })}
-              </ul>
-              <div className="totalHours">
-                Total Hours:
-                <div className="totalNum">
-                  {totalHours(weekday.finishedItems)}
+      <div className="weekBarTrack">
+        <AnimatePresence initial={false} custom={direction}>
+          <motion.div
+            className="weekBar"
+            key={moment(currentWeek).week()}
+            custom={direction}
+            variants={variants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{
+              x: { type: "spring", stiffness: 400, damping: 100 },
+              opacity: { duration: 0.2 },
+            }}
+          >
+            {weekCards.map((weekday) => {
+              return (
+                <div className="weekCard" key={weekday.id}>
+                  <label className={`days ${weekday.day}`}>{weekday.day}</label>
+                  <ul className="weekList">
+                    {weekday.finishedItems.map((item) => {
+                      return (
+                        <li className="weekListItem" key={item.id}>
+                          {item.todo}
+                          <div className="numHours">{item.completionTime}</div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                  <div className="totalHours">
+                    Total Hours:
+                    <div className="totalNum">
+                      {totalHours(weekday.finishedItems)}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          );
-        })}
+              );
+            })}
+          </motion.div>
+        </AnimatePresence>
       </div>
     </div>
   );
